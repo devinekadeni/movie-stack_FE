3b0bd6027619b9ae00f59bcf35726fc3dbcb61aa
Devin <devinekadeni@gmail.com>
2020-06-16 08:29:55 +0700

    initial commit along with library & configuration

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  const isProduction = api.env('production');
+  // api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    'react-hot-loader/babel',
+    [
+      'babel-plugin-styled-components',
+      {
+        ssr: false,
+        displayName: !isProduction,
+      },
+    ],
+  ];
+
+  return { presets, plugins };
+};
