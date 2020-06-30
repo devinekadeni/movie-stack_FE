1bb84222e3d61e5b0b9682538238f0144804f3ac
Devin <devinekadeni@gmail.com>
2020-06-30 23:38:06 +0700

    implement basic react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
-  return <div>Hello World</div>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div>Hello World</div>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
