e6ad6724ea6c928110ee730387c03ca1d240888c
Devin <devinekadeni@gmail.com>
2020-08-02 17:53:35 +0700

    add server dev configuration for handling react-router server side

--- server/development.server.js
+++ server/development.server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
@@ -6,13 +7,25 @@ function initializeDevServer(app) {
   const config = require('../webpack/webpack.dev')
   const compiler = webpack(config)
 
-  app.use(
-    webpackDevMiddleware(compiler, {
-      publicPath: config.output.publicPath,
-    })
-  )
+  const webpackMiddleware = webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  })
 
+  app.use(webpackMiddleware)
   app.use(webpackHotMiddleware(compiler))
+
+  const fs = webpackMiddleware.fileSystem
+
+  app.get('*', (req, res) => {
+    const filePath = path.join(compiler.outputPath, 'index.html')
+    fs.readFile(filePath, (err, file) => {
+      if (err) {
+        res.sendStatus(404)
+      } else {
+        res.send(file.toString())
+      }
+    })
+  })
 }
 
 module.exports = initializeDevServer
