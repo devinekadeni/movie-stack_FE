bf13f358227f46146ff303d2a9702509357b62c4
Devin <devinekadeni@gmail.com>
2020-09-22 21:28:28 +0700

    style: Fix MuiTheme global

--- styles/MUI-theme.ts
+++ styles/MUI-theme.ts
@@ -2,6 +2,21 @@ import { createMuiTheme } from '@material-ui/core/styles'
 import { BASIC_COLOR } from '@/styles/_colors'
 
 const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        html: {
+          /* will set the rem value = 10px on browser configuration font-size medium (recommended by browser) */
+          fontSize: '0.625rem',
+        },
+        body: {
+          fontFamily: "'Ubuntu', sans-serif",
+          fontSize: '1.4rem',
+          backgroundColor: '#fff',
+        },
+      },
+    },
+  },
   palette: {
     primary: {
       main: BASIC_COLOR.mainGreen,
