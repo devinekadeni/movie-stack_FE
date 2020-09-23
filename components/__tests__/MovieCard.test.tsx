b240fc3bdf26ed11b93f333133da13fecb7a0fd3
Devin <devinekadeni@gmail.com>
2020-09-23 23:35:05 +0700

    test: Add unit test on MovieCard component

---
+++ components/__tests__/MovieCard.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@/helpers/test-utils'
+import MovieCard from '@/components/MovieCard'
+
+test('should render MovieCard properly', () => {
+  const props = {
+    poster: 'https://image.tmdb.org/t/p/w185/cjr4NWURcVN3gW5FlHeabgBHLrY.jpg',
+    rating: 73,
+  }
+
+  render(<MovieCard {...props} />)
+  expect(screen.getByAltText('poster image')).toHaveAttribute('src', props.poster)
+  expect(screen.getByText((props.rating / 10).toFixed(1))).toBeInTheDocument()
+})
