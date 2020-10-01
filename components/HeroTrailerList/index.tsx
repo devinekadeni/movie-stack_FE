5c6d6d280826c42be0b86bdb28ae97ff1cd2f3a8
Devin <devinekadeni@gmail.com>
2020-10-02 02:36:13 +0700

    feat: Add heroTrailerList component

---
+++ components/HeroTrailerList/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import HeroTrailer from '@/components/HeroTrailer'
+
+const Wrapper = styled.div`
+  padding-left: 20px;
+  white-space: nowrap;
+  overflow: hidden;
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  /* Hide scrollbar for IE, Edge and Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+`
+
+const HeroWrapper = styled.div`
+  display: inline-block;
+  padding-right: 20px;
+  vertical-align: middle;
+`
+
+interface Trailers {
+  id: string
+  url: string
+  title: string
+  summary: string
+}
+
+interface Props {
+  trailers: Trailers[]
+  muted: boolean
+}
+
+const HeroTrailerList: React.FC<Props> = ({ trailers, muted }) => {
+  const [selectedId, setSelectedId] = useState(trailers[0].id)
+
+  return (
+    <Wrapper>
+      {trailers.map((val) => {
+        return (
+          <HeroWrapper key={val.id} onClick={() => setSelectedId(val.id)}>
+            <HeroTrailer
+              trailerData={{
+                id: val.id,
+                url: val.url,
+                title: val.title,
+                summary: val.summary,
+              }}
+              id={val.id}
+              isSelected={selectedId === val.id}
+              muted={muted}
+              isItemList
+            />
+          </HeroWrapper>
+        )
+      })}
+    </Wrapper>
+  )
+}
+
+export default HeroTrailerList
