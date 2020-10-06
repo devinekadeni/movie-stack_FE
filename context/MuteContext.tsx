f123d1e791e0a222eaca3a4f83136d285a120f8b
Devin <devinekadeni@gmail.com>
2020-10-06 22:50:58 +0700

    chore: Add MuteContext

---
+++ context/MuteContext.tsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react'
+
+type ContextState = [boolean, (state: boolean) => void]
+
+const initialValue = true
+
+export const MuteContext = createContext<ContextState>([initialValue, () => null])
+
+export const MuteContextProvider: React.FC = (props) => {
+  const [isMute, setIsMute] = useState(initialValue)
+  return (
+    <MuteContext.Provider value={[isMute, setIsMute]}>
+      {props.children}
+    </MuteContext.Provider>
+  )
+}
+
+export default MuteContextProvider
