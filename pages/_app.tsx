7d67871b0dc1d88526eeea5dcba64912b950c769
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2020-12-05 12:25:38 +0700

    feat: Setup ApolloClient provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app'
 import React, { useEffect, ReactNode } from 'react'
+import { ApolloProvider } from '@apollo/client'
 
 import { MuteContextProvider } from '@/context/MuteContext'
+import { useApollo } from '@/services/ApolloClient'
 
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -10,6 +12,8 @@ import MuiTheme from '@/styles/MUI-theme'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactNode => {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
@@ -19,15 +23,17 @@ const MyApp = ({ Component, pageProps }: AppProps): ReactNode => {
   }, [])
 
   return (
-    <ThemeProvider theme={MuiTheme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <MuteContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </MuteContextProvider>
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider theme={MuiTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <MuteContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MuteContextProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
