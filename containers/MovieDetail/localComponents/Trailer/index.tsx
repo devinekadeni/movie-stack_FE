d026d66c6f6312326294dafbdd36febc50bbebe1
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2020-12-30 17:41:02 +0700

    feat: Add localComponent Trailer on MovieDetail page

---
+++ containers/MovieDetail/localComponents/Trailer/index.tsx
@@ -0,0 +1,25 @@
+import { Wrapper, Layer, PlayIcon, TrailerName } from './styles'
+
+interface Props {
+  url: string
+  name: string
+}
+
+const Trailer: React.FC<Props> = ({ url, name }) => {
+  const youtubeId = url.split('watch?v=')[1]
+
+  return (
+    <Wrapper>
+      <img
+        alt="Trailer Thumbnail"
+        src={`https://img.youtube.com/vi/${youtubeId}/0.jpg`}
+      />
+      <Layer>
+        <PlayIcon color="#fff" size="4em" />
+        <TrailerName>{name}</TrailerName>
+      </Layer>
+    </Wrapper>
+  )
+}
+
+export default Trailer
---
+++ containers/MovieDetail/localComponents/Trailer/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import { PlayCircleOutline } from '@styled-icons/material/PlayCircleOutline'
+
+export const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  transition: border 0.3s;
+  cursor: pointer;
+  min-height: 156px;
+
+  &:hover {
+    border: 1px solid #11cfbc;
+  }
+
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+`
+
+export const Layer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+`
+
+export const PlayIcon = styled(PlayCircleOutline)`
+  position: absolute;
+  right: 50%;
+  bottom: 50%;
+  transform: translate(50%, 50%);
+`
+
+export const TrailerName = styled.span`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  font-weight: bold;
+  color: #fff;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  padding: 0 12px 8px;
+`
