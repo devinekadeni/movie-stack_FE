38b02430aa3e8625f160c6e5a88f492acc4b0830
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2021-01-03 23:45:08 +0700

    feat: Add localComponent dialogMedia on movieDetail page

---
+++ containers/MovieDetail/localComponents/DialogMedia/index.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import ReactPlayer from 'react-player'
+import Dialog from '@material-ui/core/Dialog'
+import { Close as CloseIcon } from '@styled-icons/material-rounded/Close'
+
+interface Props {
+  isOpen: boolean
+  onClose: () => void
+  type: string
+  url: string
+  renderControl?: React.ReactNode
+}
+
+const CloseButton = styled.span`
+  position: fixed;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+  color: #e3e3e3;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
+
+const ImageWrapper = styled.div`
+  img {
+    object-fit: cover;
+  }
+`
+
+const DialogMedia: React.FC<Props> = ({ isOpen, onClose, type, url, renderControl }) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose} maxWidth={false}>
+      <>
+        <CloseButton onClick={onClose} title="Close Media">
+          <CloseIcon size="3em" />
+        </CloseButton>
+        {renderControl}
+        {type === 'image' && (
+          <ImageWrapper>
+            <img src={url} alt="Dialog detail" />
+          </ImageWrapper>
+        )}
+        {type === 'video' && (
+          <ReactPlayer url={url} playing controls width="90vw" height="90vh" />
+        )}
+      </>
+    </Dialog>
+  )
+}
+
+export default DialogMedia
