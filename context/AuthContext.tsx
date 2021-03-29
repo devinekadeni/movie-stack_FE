ed51565a3d77fa1632a8fe0232487210b97677ff
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2021-03-29 23:32:26 +0700

    feat: Update AuthContext to maintain user session data

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -4,26 +4,49 @@ export type Genre = { id: string; name: string }
 
 type AuthType = 'sign-in' | 'sign-up'
 
+interface User {
+  id: string
+  name: string
+  email: string
+}
+
 interface State {
   isOpen?: boolean
   authType: AuthType
+  isLoggedIn: boolean
+  user: User
 }
 
-type Action = { type: 'OPEN_SIGN_IN' | 'OPEN_SIGN_UP'; payload: boolean }
+type Action =
+  | { type: 'OPEN_SIGN_IN'; payload: boolean }
+  | { type: 'OPEN_SIGN_UP'; payload: boolean }
+  | { type: 'SET_USER_SESSION'; payload: { isLoggedIn: boolean; user: User } }
 
 type ContextState = [State, (state: Action) => void]
 
 const initialState = {
   isOpen: false,
   authType: 'sign-in' as AuthType,
+  isLoggedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+  },
 }
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
     case 'OPEN_SIGN_IN':
-      return { isOpen: action.payload, authType: 'sign-in' }
+      return { ...state, isOpen: action.payload, authType: 'sign-in' }
     case 'OPEN_SIGN_UP':
-      return { isOpen: action.payload, authType: 'sign-up' }
+      return { ...state, isOpen: action.payload, authType: 'sign-up' }
+    case 'SET_USER_SESSION':
+      return {
+        ...state,
+        user: action.payload.user,
+        isLoggedIn: action.payload.isLoggedIn,
+      }
     default:
       return { ...state }
   }
