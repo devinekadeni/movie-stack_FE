9461146eb1bd808dccbb73a98ae083f76d442495
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2021-04-07 23:31:24 +0700

    chore: Set dynamic import sign in & sign up component to false

--- containers/Authentication/index.tsx
+++ containers/Authentication/index.tsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react'
 import { AuthContext } from '@/context/AuthContext'
 import dynamic from 'next/dynamic'
-const SignIn = dynamic(() => import('./SignIn'))
-const SignUp = dynamic(() => import('./SignUp'))
+const SignIn = dynamic(() => import('./SignIn'), { ssr: false })
+const SignUp = dynamic(() => import('./SignUp'), { ssr: false })
 
 const Authentication: React.FC = () => {
   // handle server side
